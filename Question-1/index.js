f6acc91be0e2182113eabb188029ea61f574886f
casey <caseymwangi31@gmail.com>
2024-05-01 21:01:53 +0300

    using the for loop

---
+++ Question-1/index.js
@@ -0,0 +1,6 @@
+//solution
+//This loop starts from 999 and decreases to 100, printing each number in decreasing order.
+//using the for loop
+for(let i = 999; i >= 100; i--){
+    console.log(i);
+}
